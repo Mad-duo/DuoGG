aff4ff3e95835c04acc882beec5af75b4c827612
jaeseok1111 <jaeseok1111@gmail.com>
2020-09-27 21:08:00 +0900

    react, webpack 셋팅

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Root from "./root";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+ReactDOM.render(<Root />, document.getElementById("root"));
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname + "/build")
+    },
+    mode: "none",
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modues/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader",
+                        options: { minimize: true }
+                    }
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader, 
+                    "css-loader"
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./public/index.html",
+            filename: "./index.html"
+        }),
+        new MiniCssExtractPlugin({
+            filename: "style.css"
+        })
+    ]
+}
