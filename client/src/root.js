957e34c909b1626dfe3d9e484644e519d004e1e9
Youngchangoon <youngchangoon521@gmail.com>
2020-10-10 18:07:47 +0900

    배경색 변경

--- client/src/root.js
+++ client/src/root.js
@@ -10,7 +10,7 @@ const Background = styled.div`
   left: 0;
   width: 100%;
   height: 100%;
-  background-color: #7D8084;
+  background-color: #FADBBD;
   /* background-image: url('../images/background.png'),
   linear-gradient(
       to right,
